4e71d64f493bfa7f48c828a83e75c1d35fb7c270
Yuriy Guts <yuriy.guts@gmail.com>
2015-08-13 16:19:00 +0300

    Minor code cleanup.

--- readData.m
+++ readData.m
@@ -1,22 +1,25 @@
 function [X, y] = readData(folder)
 
-wordFiles = getAllFilesInFolder(strcat(folder, '/'), '*.wav');
-samplesPerFile = size(mapWaveformToFeatures(audioread(wordFiles{2})), 1);
+wordFiles = getAllFilesInFolder(strcat(folder, '/'), 'pronunciation*.wav');
+featuresPerFile = size(mapWaveformToFeatures(audioread(wordFiles{2})), 1);
 
 % Preallocate memory.
-X = zeros(length(wordFiles), samplesPerFile);
+X = zeros(length(wordFiles), featuresPerFile);
 y = zeros(length(wordFiles), 1);
 
 dataRowIndex = 1;
 
-% Read each file, extract the waveform and the class name.
+% Read each file, extract the features and the correct answer.
 for file = wordFiles'
     fileName = file{1};
     wordMatch = regexp(fileName, 'pronunciation_en_([a-z]+)', 'tokens');
     word = wordMatch{1}{1};
 
+    % Read waveform and convert it to a feature vector.
     waveform = audioread(fileName);
     X(dataRowIndex, :) = mapWaveformToFeatures(waveform);
+    
+    % If the filename contains 'google', the answer for this file is 1.
     y(dataRowIndex) = strcmp(word, 'google');
 
     dataRowIndex = dataRowIndex + 1;
